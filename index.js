d3b38d17a123f52240a6048982eeac809e6223da
PushkaryovVD <vadimka-pushkarev@mail.ru>
2021-06-21 15:30:15 +0600

    1-4 task complete

--- index.js
+++ index.js
@@ -5,35 +5,65 @@ let getVowels = (str) => str.replace(/[првткдл!?]/gi, '');
 console.log(getVowels(lower));
 const string = "Привет! Как дела?";
 const vowels=["а","е","ё","и","о","у","ы","э","ю","я"];
-const getVowels= stringFilter =>{
-    let result="";
-    for (let i =0;i<stringFilter.lenght;i++){
-        const currentLetter = stringFilter[i].toLowerCase();
-        if(vowels.includes(currentLetter)){
-            result +=currentLetter;
-        }
+const getVowels = stringToFilter => {
+    let extractedVowels = "";
+    for (let i = 0; i < stringToFilter.length; i++) {
+    const currentLetter = stringToFilter[i].toLowerCase();
+
+    if (vowels.includes(currentLetter)) {
+        extractedVowels += currentLetter;
     }
-    return result;
+    }
+
+    return extractedVowels;
 }
+
 console.log(getVowels(string));
 // Задача2
 const workers = [
     {"name":"John","salary":500},
     {"name":"Mike","salary":1300},
     {"name":"Linda","salary":1500}];
-
+    const getWorthyWorkers = (workersArr) => {
+        const worthyWorkers = [];
+        workersArr.forEach(currentWorker => {
+        if (currentWorker.salary > 1000) {
+            worthyWorkers.push(currentWorker.name)
+        }
+        })
+    
+        return worthyWorkers;
+    };
     
 console.log(getWorthyWorkers(workers))
 // Задача3
 const path = "/users/download/index.html"
 
-console.log(isHtml(path))
+const isHtml = path => {
+const requiredExt = ".html";
+const pathExt = path.slice(-5);
+
+return pathExt === requiredExt
+}
+
+console.log(isHtml(path));
 // Задача4
-const mixedArray = [3,13,74,14,66,15,22,4];
-const isEven = num => { /* code */ };
+const mixedArray = [3, 13, 74, 14, 66, 15, 22, 4];
+
+const isEven = num => {
+return num % 2 === 0;
+}
+
+const filterArray = ( arrayToFilter, filterFn ) => {
+const filteredArray = [];
+
+arrayToFilter.forEach(num => {
+    if (filterFn(num)) {
+    filteredArray.push(num);
+    }
+})
+
+return filteredArray;
+}
+
 console.log(filterArray(mixedArray, isEven));
-
-// ДОМ1
-
-
-// ДОМ2
